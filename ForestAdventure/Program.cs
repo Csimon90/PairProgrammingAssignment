c51ccadbd4282353490c13353235f049f7edcf43
Caitlin Simon <csimondesign@gmail.com>
2022-11-03 18:10:00 -0400

    Intro  to game

--- ForestAdventure/Program.cs
+++ ForestAdventure/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Forest Adventure!");
+            Console.WriteLine("Your head is pounding. Light from the sun above is blinding, as you first open your eyes. After a moment your eyes adjust and you realize you have woken up deep with in the old growth forest. Just as the panic of being alone in the woods begins to bubble you notice three items laying at your feet. And a scroll that reads:  For all your riches and petty greed, a troubled path you shall lead. Only one favor I shall treat, select one object by your feet."
+            Console.ReadKey();
+            );
+        }
+    }
+}
+
