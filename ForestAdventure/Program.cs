bbed36142e08bdfbb7096f98175ef5893e16240f
Caitlin Simon <csimondesign@gmail.com>
2022-11-03 18:39:39 -0400

    edited for grammar and punctuation.

--- ForestAdventure/Program.cs
+++ ForestAdventure/Program.cs
@@ -11,9 +11,19 @@ namespace Adventure
         static void Main(string[] args)
         {
             Console.WriteLine("Forest Adventure!");
-            Console.WriteLine("Your head is pounding. Light from the sun above is blinding, as you first open your eyes. After a moment your eyes adjust and you realize you have woken up deep with in the old growth forest. Just as the panic of being alone in the woods begins to bubble you notice three items laying at your feet. And a scroll that reads:  For all your riches and petty greed, a troubled path you shall lead. Only one favor I shall treat, select one object by your feet."
+
+            Console.WriteLine("Your head is pounding. Light from the sun above the clearing is blinding as you first open your eyes."); 
+            Console.WriteLine("After a moment you adjust and you realize you have woken up deep within an old growth forest."); 
+            Console.WriteLine("You do not remember how you came to be at this place.");
+            Console.WriteLine("You begin to realize you don't even know your own name.");
+            Console.WriteLine("Just as the panic of being alone in the woods begins to bubble you notice three items and a weathered scroll with shaky,");
+            Console.WriteLine("scratched writing, laying at your feet.");
+            Console.WriteLine("You unroll and begin to read:  'For all your riches and petty greed, a troubled path you shall lead."); 
+            Console.WriteLine("Only one favor I will treat, select one object by your feet.'");
+            Console.WriteLine("As you read the last line the scroll burst to flame and leaves nothing but ash and a whisper of smoke.");
+            Console.WriteLine("You turn your attention to the objects before you.");
+            Console.WriteLine("What do you choose?");
             Console.ReadKey();
-            );
         }
     }
 }
