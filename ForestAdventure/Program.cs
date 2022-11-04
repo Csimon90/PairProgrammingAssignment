f1dfc8bf9dbc7a75254937d6046bd74a1f80469b
Caitlin Simon <csimondesign@gmail.com>
2022-11-03 20:35:54 -0400

    Class types broken up and story line started

--- ForestAdventure/Program.cs
+++ ForestAdventure/Program.cs
@@ -27,12 +27,9 @@ namespace ForestAdventure
             //allows for user input to be read and stored
             WeaponChoice = Console.ReadLine();
 
-            Console.WriteLine("You pick up " + WeaponChoice + ". You pray you've made the right decision,");
-            Console.WriteLine("upon picking up your choice the other two disappear.");
-            Console.WriteLine("Just as the scroll before them.");
-            Console.WriteLine("With great shaking and rumbling a path opens up in the woods as the trees pop and groan to bend around it.");
-            Console.WriteLine("The path is dark. There is no way to know where it leads but you have no other options.");
-            Console.WriteLine("You take a breath. Steady your courage and make for the path.");
+
+
+
         Console.ReadKey();
         }
     }
---
+++ ForestAdventure/TheSword.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+namespace ForestAdventure
+{
+    class TheSword
+    {
+        
+        static void Main()
+        {
+            string WeaponChoice = " The Sword, The Bow, The Wand";
+
+System.Console.WriteLine("You pick up " + WeaponChoice + ". You pray you've made the right decision,");
+            Console.WriteLine("upon picking up your choice the other two disappear.");
+            Console.WriteLine("Just as the scroll before them.");
+            Console.WriteLine("With great shaking and rumbling a path opens up in the woods as the trees pop and groan to bend around it.");
+            Console.WriteLine("The path is dark. There is no way to know where it leads but you have no other options.");
+            Console.WriteLine("You take a breath. Steady your courage and make for the path.");
+
+
+
+
+
+        }
+    }
+}
